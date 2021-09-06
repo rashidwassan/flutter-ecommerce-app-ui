fc82b960dfd6a0a753e39b79221c71fdbb4bc06e
Rashid Wassan <rashidwassaan@gmail.com>
2021-09-06 23:37:13 +0500

    Added native splash screens

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,5 +1,7 @@
 class Images {
   Images._();
 
+  static const String logo = 'assets/icons/logo.png';
+
   static const String loginBg = 'assets/images/login_bg.png';
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:ecommerce_app_isaatech/constants/images.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,12 +16,33 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(8),
-        child: Image.asset(
-          Images.loginBg,
-          fit: BoxFit.cover,
-        ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage(
+                Images.loginBg,
+              ),
+              fit: BoxFit.cover,
+            )),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: 2.0,
+              sigmaY: 2.0,
+            ),
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+          Center(
+            child: ClipRRect(
+                    borderRadius: BorderRadius.circular(500),
+                    child: Image.asset(Images.logo))
+                .p(32),
+          )
+        ],
       ),
     );
   }
