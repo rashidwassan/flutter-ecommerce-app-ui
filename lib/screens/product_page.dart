54d8207e662d991f4368250299cc836a965d98ef
Rashid Wassan <rashidwassaan@gmail.com>
2021-09-30 23:18:44 +0500

    .

---
+++ lib/screens/product_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class ProductPage extends StatefulWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
